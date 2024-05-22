41338c93ce2380d09f27bdf2871c9cd7c184a536
Ibrahim-2003 <ialakash@live.com>
2024-05-22 15:27:21 -0500

    Work Station Update

--- IbrahimStarlingUI/app/src/main/java/com/example/ibrahimstarlingui/Screen.kt
+++
@@ -1,7 +0,0 @@
-package com.example.ibrahimstarlingui
-
-sealed class Screen(val route: String) {
-    object MainScreen : Screen("main_screen")
-    object HealthScreen : Screen("health_screen")
-    object ProfileScreen : Screen("profile_screen")
-}
